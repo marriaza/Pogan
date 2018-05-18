fc94c75c3a50862d1e330bb9bfa2daa05e405a81
Morgan <mo_gies97@hotmail.com>
2018-05-18 14:35:40 -0600

    Se sube

---
+++ Pogan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pogan.Startup))]
+namespace Pogan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
